996e5967e5ddb4eb6c4a9526a085e861b347756c
vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-06 10:14:12 +0000

    changes

--- user_input.c
+++ user_input.c
@@ -5,6 +5,12 @@ int main(){
 	printf("Enter your Name: \n");
 	scanf("%s",name);
 	printf("your name is: %s", name);
+	if (name == "Vikee"){
+		printf("You are the owner of this code");
+	}
+	else{
+		printf("You are not the owner of this code");
+	}
 	return 0;
 
 }
