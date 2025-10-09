e08abd4477b8afc477a11101aaab838a88730a34
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-09 23:30:47 +0530

    datatypes

--- Variables-DataTypes/Datatype01.c
+++ Variables-DataTypes/Datatype01.c
@@ -2,6 +2,10 @@
 
 void main(){
 	
-	printf("%ld\n", sizeof(int));
+	printf("%ld\n", sizeof(int));   //4
+	printf("%ld\n", sizeof(float));
+	printf("%ld\n", sizeof(char));
+	printf("%ld\n", sizeof(double));
+	printf("%ld\n", sizeof(long));
 
 }
