17abe9ef53068635c78edf89d6bd1546982bd137
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-12 16:01:21 +0530

    Compilation_Stages01

---
+++ Compilation_Stages/Program01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>   //<> : find in systeam folders
+#include "stdio.h"		     //"" : find in current director
+
+
+void main(){
+
+	printf("Hello i'am vikee\n");
+}
--- Variables-DataTypes/Datatype01.c
+++ Variables-DataTypes/Datatype01.c
@@ -2,10 +2,10 @@
 
 void main(){
 	
-	printf("%ld\n", sizeof(int));   //4
-	printf("%ld\n", sizeof(float));
-	printf("%ld\n", sizeof(char));
-	printf("%ld\n", sizeof(double));
-	printf("%ld\n", sizeof(long));
+	printf("%ld\n", sizeof(int));   	//4
+	printf("%ld\n", sizeof(float));		//4
+	printf("%ld\n", sizeof(char));		//1
+	printf("%ld\n", sizeof(double));	//8
+	printf("%ld\n", sizeof(long));		//8
 
 }
