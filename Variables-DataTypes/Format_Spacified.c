1110ba917d8c5b7572a592a79684f0e5203c44c9
vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-14 09:54:30 +0000

    Format_Spacified

---
+++ Variables-DataTypes/Format_Spacified.c
@@ -0,0 +1,23 @@
+/*
+ int %d
+ float %f
+ char %c
+ double %lf
+ long %ld
+ */
+
+#include <stdio.h>
+
+void main(){
+	int value = 25;
+	float height = 123.4;
+	char alphabet = 'A';
+       	double PI = 3.1434939;
+	long data = 23493894;
+
+	printf("value is : %d\n", value);
+	printf("height is : %f\n", height);
+	printf("alphabet is : %c\n", alphabet);
+	printf("PI is : %lf\n", PI);
+	printf("data is : %ld\n", data);	
+}
