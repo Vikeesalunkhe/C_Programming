90bc15d1b09eac7979b9eb39770fc52f22495ade
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-15 23:12:14 +0530

    Arithmatics_and_Relatinol_Operators

---
+++ Operators/Arithmatics1.c
@@ -0,0 +1,23 @@
+//Arithmatics Operators
+/*
+	+ : Addition
+	- : Substarction
+	* : Multiplication
+	/ : Division
+	% : Modulus
+*/
+
+
+#include <stdio.h>
+
+void main() {
+	
+	int x = 10;
+	int y = 20;
+
+	printf("x + y = %d\n", x+y);        //30
+	printf("x - y = %d\n", x-y);		//-10
+	printf("x * y = %d\n", x*y);		//200
+	printf("x / y = %d\n", x/y);		//0
+	printf("x % y = %d\n", x%y);		//10
+}
