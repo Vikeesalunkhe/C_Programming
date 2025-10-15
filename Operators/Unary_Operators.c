dfbe46c1b1f966b16aba046b5383c978dd9a481c
vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-15 04:32:17 +0000

    pre_increment

--- Operators/Unary_Operators.c
+++ Operators/Unary_Operators.c
@@ -16,7 +16,7 @@ void main() {
 	x = 10;
         ans = 0;
         printf("x is : %d\n", x);       //10
-        ans = ++x + ++x;
+        ans = ++x + ++x;                // (x=10 , pre_x=11) + (x=11 , pre_x=12)
         printf("x is : %d\n", x);       //12
         printf("ans is : %d\n", ans);   //24
 
@@ -25,7 +25,7 @@ void main() {
 	int y = 10;
         ans = 0;
         printf("y is : %d\n", y);       //10
-        ans = y++;
+        ans = y++;                      // (x=10 , post_x=11) 
         printf("y is : %d\n", y);       //11
         printf("ans is : %d\n", ans);   //10
 	
@@ -36,6 +36,4 @@ void main() {
         ans = y++ + y++;                //(x=10 , post_x=11) + (x=11 , post_x=12)
         printf("y is : %d\n", y);       //12
         printf("ans is : %d\n", ans);   //21
-
-	
 }
