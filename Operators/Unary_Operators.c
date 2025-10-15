dd79e1da639fc8374811edfaf5a65fedd0ea68e8
vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-15 04:24:08 +0000

    pre_post_increment

--- Operators/Unary_Operators.c
+++ Operators/Unary_Operators.c
@@ -11,6 +11,16 @@ void main() {
 	printf("x is : %d\n", x);      //11
 	printf("ans is : %d\n", ans);   //11
 				 
+	////////
+	
+	x = 10;
+        ans = 0;
+        printf("x is : %d\n", x);      //10
+        ans = ++x + ++x;
+        printf("x is : %d\n", x);      //12
+        printf("ans is : %d\n", ans);   //24
+
+
 	//post-increment
 	int y = 10;
         ans = 0;
@@ -18,6 +28,14 @@ void main() {
         ans = y++;
         printf("y is : %d\n", y);      //11
         printf("ans is : %d\n", ans);   //10
+	
+	//////
+	y = 10;
+        ans = 0;
+        printf("y is : %d\n", y);      //10
+        ans = y++ + y++;
+        printf("y is : %d\n", y);      //12
+        printf("ans is : %d\n", ans);   //24
 
 	
 }
