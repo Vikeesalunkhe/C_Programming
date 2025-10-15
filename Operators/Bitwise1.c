990d9c88dbcffcbf986dc3427acf635054b0aeae
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-15 23:27:25 +0530

    Bitwise

---
+++ Operators/Bitwise1.c
@@ -0,0 +1,20 @@
+//Bitwise Operators
+
+/*
+    & : Bitwise AND
+    | : Bitwise OR
+    ^ : 
+    << :
+    >> :
+*/
+
+#include <stdio.h>
+
+void main() {
+
+    int x = 10;
+    int y = 20;
+
+    printf("x & y : %d\n", x&y);
+    printf("x | y : %d\n", x|y);
+}
