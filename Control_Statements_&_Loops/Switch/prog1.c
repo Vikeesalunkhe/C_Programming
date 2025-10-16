01356a0b5d63b2490bd8b6a25317127a09e216c3
vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-16 11:06:12 +0000

    switch

---
+++ Control_Statements_&_Loops/Switch/prog1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void main() {
+	
+	int number = 6;
+
+	switch (number) {
+		
+		case 1:{
+			printf("one");
+			}
+			break;
+		case 2:{
+		       printf("Two");
+			}
+		       break;
+
+		case 3 :{
+			printf("Three");
+			}
+			break;
+
+		case 4 :{
+			printf("Four");
+			}
+			break;
+
+		default :
+			printf("invalid");
+
+	}	
+}
