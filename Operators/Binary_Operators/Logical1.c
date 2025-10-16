0ee67a3565ed0630ee2d6f71e8d32dcb42dd001b
vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-10-16 05:48:47 +0000

    Changes

--- Operators/Binary_Operators/Logical1.c
+++ Operators/Binary_Operators/Logical1.c
@@ -13,14 +13,14 @@ void main() {
     int x = 10;
     int y = 20;
 
-    printf("x && y : %d\n", x&&y);
-    printf("x || y : %d\n", x||y);
+    printf("x && y : %d\n", x&&y);      //1
+    printf("x || y : %d\n", x||y);      //1
 
-    printf("1 && 0 : %d\n", 1 && 0);
-    printf("1 || 0 : %d\n", 1 || 0);
+    printf("1 && 0 : %d\n", 1 && 0);    //0
+    printf("1 || 0 : %d\n", 1 || 0);    //1
 
 
-    printf("0 && 0 : %d\n", 0 && 0);
-    printf("0 || 0 : %d\n", 0 || 0);
+    printf("0 && 0 : %d\n", 0 && 0);    //0
+    printf("0 || 0 : %d\n", 0 || 0);    //0
 
 }
