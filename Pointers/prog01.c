56e89b53e88b655dc2b080f774212a1e126307df
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-11-01 21:12:22 +0530

    pointer

--- Pointers/prog01.c
+++ Pointers/prog01.c
@@ -2,10 +2,23 @@
 
 void main() {
 
-	int num = 10;
+	int num = 10;    //declaration
+	int *ptr = &num;   //store address of num variable //pointer Referencing
 
-	int *ptr = &num;   //store address of num variable
-	
-	
-	printf("Pointer of Num variable is : %d\n", *ptr);
+	printf("%d\n", num);
+	printf("%p\n", &num);
+	printf("%p\n", ptr);
+	printf("%d\n", *ptr);
+
+	printf("------------------------\n");
+
+	int num02;       //defenatoion
+	num02 = 20;      //Assignment
+	int *ptr_num02;   //wild pointer
+	ptr_num02 = &num02;
+
+	printf("pointer value is : %d\n", *ptr_num02);
+	printf("Addres of num02 : %p\n", ptr_num02);
+
+
 }
