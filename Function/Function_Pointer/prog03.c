797041eee3603de68f9a434566fba12bf54fefd7
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-11-19 11:16:37 +0000

    funtion pointer

--- Function/Function_Pointer/prog03.c
+++ Function/Function_Pointer/prog03.c
@@ -2,5 +2,30 @@
 
 int sum(int x, int y) {
 
-    return 
+    return x+y;
+}
+
+int sub(int x, int y) {
+
+    return x-y;
+}
+
+int mul(int x, int y) {
+
+    return x*y;
+}
+
+int div(int x, int y) {
+
+    return x/y;
+}
+
+void main() {
+
+    int (*ptr[4])(int,int)= {sum,sub,mul,div};
+
+    for (int i = 0; i<4; i++) {
+
+        printf("%d\n", ptr[i](20, 10));
+    }
 }
