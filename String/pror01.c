e5df82eeaadb40c31d2f1f4205c5b81ebbedcc44
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-11-30 21:21:27 +0530

    delete unused files

---
+++ String/pror01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+
+    char carr[] = {'V', 'i', 'k', 'e', 'e'};
+    char *str = "vikee";
+
+    printf("%ld\n", sizeof(carr));        //5
+    printf("%ld\n", sizeof(carr[0]));     //1
+
+
+
+}
