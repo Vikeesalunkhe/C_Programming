c66698501778c98e30bc660e1a2957a9fd1ecdbc
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2025-12-03 06:59:00 +0000

    Add program to find maximum between two distinct numbers

---
+++ Assignment/Control_Statements_and_Loops/if_else_statement_02/prog02.c
@@ -0,0 +1,23 @@
+// Write a program to find maximum between two distint number.
+
+#include <stdio.h>
+
+void main() {
+    int num1, num2;
+    printf("Enter num1 : ");
+    scanf("%d", &num1);
+    printf("Enter num2 : ");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("%d is maximum between %d and %d\n", num1, num1, num2);
+    }
+    else if (num1 == num2) {
+
+        printf("Given Numbers Are Equals\n");
+    }
+    else {
+        printf("%d is maximum between %d and %d\n", num2, num1, num2);
+    }
+}
+
