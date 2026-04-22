6b2203ab6aabd7e8c3e4a86f37e4dc5afc756698
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2026-04-22 09:25:56 +0000

    solve string token

--- String/my_strtok.c
+++ String/my_strtok.c
@@ -10,6 +10,7 @@ char *my_strtok(char *str, const char *delim){
 
     if (str != NULL){   //first time onlyy execute
         ptr = str;
+        i=0;
     }
 
     while (ptr[i] != '\0'){
@@ -20,6 +21,28 @@ char *my_strtok(char *str, const char *delim){
             if (ptr[i] == delim[j]){
                 ptr[i] = '\0';
                 i++;
+
+                
+                // Skip multiple delimiters
+                while (ptr[i] != '\0')
+                {
+                    int k = 0, is_delim = 0;
+
+                    while (delim[k] != '\0')
+                    {
+                        if (ptr[i] == delim[k])
+                        {
+                            is_delim = 1;
+                            break;
+                        }
+                        k++;
+                    }
+
+                    if (!is_delim)
+                        break;
+
+                    i++;
+                }
                 return &ptr[start];
             }
             else{
@@ -44,11 +67,97 @@ int main(){
     char delim[] = ";:";
 
     char *token = my_strtok(str, delim);
-    printf("%s \n", token);
 
     while (token != NULL){
-        token = my_strtok(NULL, delim);
         printf("%s \n", token);
+        token = my_strtok(NULL, delim);
+        
     }
 
 }
+
+
+// #include <stdio.h>
+// #include <string.h>
+
+// char *my_strtok(char *str, const char *delim)
+// {
+//     static char *ptr;
+//     static int i = 0;
+//     int j, start;
+
+//     // First call
+//     if (str != NULL)
+//     {
+//         ptr = str;
+//         i = 0;
+//     }
+
+//     // If end of string reached
+//     if (ptr[i] == '\0')
+//         return NULL;
+
+//     start = i;
+
+//     while (ptr[i] != '\0')
+//     {
+//         j = 0;
+
+//         // Check if current char is delimiter
+//         while (delim[j] != '\0')
+//         {
+//             if (ptr[i] == delim[j])
+//             {
+//                 ptr[i] = '\0';
+//                 i++;
+
+//                 // Skip multiple delimiters
+//                 while (ptr[i] != '\0')
+//                 {
+//                     int k = 0, is_delim = 0;
+
+//                     while (delim[k] != '\0')
+//                     {
+//                         if (ptr[i] == delim[k])
+//                         {
+//                             is_delim = 1;
+//                             break;
+//                         }
+//                         k++;
+//                     }
+
+//                     if (!is_delim)
+//                         break;
+
+//                     i++;
+//                 }
+
+//                 return &ptr[start];
+//             }
+//             j++;
+//         }
+//         i++;
+//     }
+
+//     // Last token
+//     if (ptr[start] != '\0')
+//         return &ptr[start];
+
+//     return NULL;
+// }
+
+// int main()
+// {
+//     char str[] = "Are;you:okay";
+//     char delim[] = ";:";
+
+//     char *token = my_strtok(str, delim);
+
+//     while (token != NULL)
+//     {
+//         printf("%s\n", token);
+//         token = my_strtok(NULL, delim);
+//     }
+
+//     return 0;
+// }
