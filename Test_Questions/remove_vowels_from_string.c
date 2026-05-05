6473909a0c2af058ed62f215f44cc015bacb52eb
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2026-05-05 05:23:29 +0000

    test question

---
+++ Test_Questions/remove_vowels_from_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void remove_vowels(char str[], char temp[]){
+
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; str[i] != '\0'; i++){
+    
+        if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || 
+            str[i] == 'O' || str[i] == 'U' || 
+            str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || 
+            str[i] == 'o' || str[i] == 'u'){
+
+            
+        }
+        else{
+            temp[j++] = str[i];
+        }
+    }
+
+    temp[j] = '\0';
+}
+
+int main(){
+
+    char str[100];
+    char temp[100];
+
+    printf("Enter string : ");
+    scanf("%[^\n]", str);
+
+    remove_vowels(str, temp);
+
+    printf("Removed vowels string is : %s\n", temp);
+}
---
+++ Test_Questions/sum-of-all-2d-array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int row, col;
+    printf("Enter row col : ");
+    scanf("%d%d", &row, &col);
+
+    printf("Enter array values : ");
+    int arr[row][col];
+    for (int i = 0; i<row; i++){
+        for (int j = 0; j<col; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i<row; i++){
+
+        for (int j = 0; j<col; j++){
+            sum += arr[i][j];
+        }
+    }
+
+
+    printf("sum : %d\n", sum);
+}
